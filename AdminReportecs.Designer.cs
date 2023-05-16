49c5857954230eea82c7ec805d34a00053565c22
MISTER angelo <mr.angelo230@outlook.com>
2023-05-16 11:04:30 -0700

    a

---
+++ AdminReportecs.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Registro_UAdeO_2023
+{
+    partial class AdminReportes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fec_inicio = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.fec_final = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rBtnHombre = new System.Windows.Forms.RadioButton();
+            this.rBtnMujer = new System.Windows.Forms.RadioButton();
+            this.rBtnOtros = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fec_inicio
+            // 
+            this.fec_inicio.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.fec_inicio.Location = new System.Drawing.Point(6, 19);
+            this.fec_inicio.Name = "fec_inicio";
+            this.fec_inicio.Size = new System.Drawing.Size(200, 20);
+            this.fec_inicio.TabIndex = 0;
+            this.fec_inicio.Value = new System.DateTime(2023, 1, 1, 0, 0, 0, 0);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.fec_final);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.fec_inicio);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(434, 195);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // fec_final
+            // 
+            this.fec_final.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.fec_final.Location = new System.Drawing.Point(212, 20);
+            this.fec_final.Name = "fec_final";
+            this.fec_final.Size = new System.Drawing.Size(200, 20);
+            this.fec_final.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fec. Inicio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(281, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fec. Fin";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rBtnOtros);
+            this.groupBox2.Controls.Add(this.rBtnMujer);
+            this.groupBox2.Controls.Add(this.rBtnHombre);
+            this.groupBox2.Location = new System.Drawing.Point(6, 72);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(313, 52);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // rBtnHombre
+            // 
+            this.rBtnHombre.AutoSize = true;
+            this.rBtnHombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rBtnHombre.Location = new System.Drawing.Point(6, 19);
+            this.rBtnHombre.Name = "rBtnHombre";
+            this.rBtnHombre.Size = new System.Drawing.Size(74, 20);
+            this.rBtnHombre.TabIndex = 0;
+            this.rBtnHombre.TabStop = true;
+            this.rBtnHombre.Text = "Hombre";
+            this.rBtnHombre.UseVisualStyleBackColor = true;
+            // 
+            // rBtnMujer
+            // 
+            this.rBtnMujer.AutoSize = true;
+            this.rBtnMujer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rBtnMujer.Location = new System.Drawing.Point(115, 19);
+            this.rBtnMujer.Name = "rBtnMujer";
+            this.rBtnMujer.Size = new System.Drawing.Size(58, 20);
+            this.rBtnMujer.TabIndex = 1;
+            this.rBtnMujer.TabStop = true;
+            this.rBtnMujer.Text = "Mujer";
+            this.rBtnMujer.UseVisualStyleBackColor = true;
+            // 
+            // rBtnOtros
+            // 
+            this.rBtnOtros.AutoSize = true;
+            this.rBtnOtros.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rBtnOtros.Location = new System.Drawing.Point(244, 19);
+            this.rBtnOtros.Name = "rBtnOtros";
+            this.rBtnOtros.Size = new System.Drawing.Size(57, 20);
+            this.rBtnOtros.TabIndex = 2;
+            this.rBtnOtros.TabStop = true;
+            this.rBtnOtros.Text = "Otros";
+            this.rBtnOtros.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnSalir);
+            this.groupBox3.Controls.Add(this.btnImprimir);
+            this.groupBox3.Location = new System.Drawing.Point(6, 139);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(183, 52);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImprimir.Location = new System.Drawing.Point(6, 19);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btnImprimir.TabIndex = 4;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(98, 19);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // AdminReportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 219);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "AdminReportes";
+            this.Text = "AdminReportecs";
+            this.Load += new System.EventHandler(this.AdminReportes_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker fec_inicio;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker fec_final;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rBtnOtros;
+        private System.Windows.Forms.RadioButton rBtnMujer;
+        private System.Windows.Forms.RadioButton rBtnHombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ AdminReportecs.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Registro_UAdeO_2023
+{
+    public partial class AdminReportes : Form
+    {
+        private SqlDataAdapter BDRegistros, BDCarrera, BDGenero;
+        private DataSet TBRegistros, TBCarrera, TBGenero;
+        private DataRow RegRegistros, RegCarrera, RegGenero;
+        private string con;
+        public AdminReportes()
+        {
+            InitializeComponent();
+        }
+        private void AdminReportes_Load(object sender, EventArgs e)
+        {
+
+            string STRSql = "SELECT ID,NomCorto FROM Carrera";
+            SqlConnection cnn = new SqlConnection(con);
+            SqlCommand cmd = new SqlCommand(STRSql, cnn);
+            BDCarrera = new SqlDataAdapter(cmd);
+            TBCarrera = new DataSet();
+            BDCarrera.Fill(TBCarrera, "Carrera");
+            RegCarrera = TBCarrera.Tables["Carrera"].Rows[0];
+            for (int i = 0; i <= BindingContext[TBCarrera, "Carrera"].Count - 1; i++)
+            {
+                BindingContext[TBCarrera, "Carrera"].Position = i;
+                RegCarrera = TBCarrera.Tables["Carrera"].Rows[i];
+            }
+            STRSql = "SELECT Id,NomGenero FROM Genero";
+            cmd = new SqlCommand(STRSql, cnn);
+            BDGenero = new SqlDataAdapter(cmd);
+            TBGenero = new DataSet();
+            BDGenero.Fill(TBGenero, "Genero");
+            RegGenero = TBGenero.Tables["Genero"].Rows[0];
+            for (int i = 0; i <= BindingContext[TBGenero, "Genero"].Count - 1; i++)
+            {
+                BindingContext[TBGenero, "Genero"].Position = i;
+                RegGenero = TBGenero.Tables["Genero"].Rows[i];
+            }
+
+        }
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void MostrarInfo()
+        {
+            string STRSql = "SELECT * FROM Registros";
+            SqlConnection cnn = new SqlConnection(con);
+            SqlCommand cmd = new SqlCommand(STRSql, cnn);
+            try
+            {
+                BDRegistros = new SqlDataAdapter(cmd);
+                BDRegistros = new SqlDataAdapter(cmd);
+                TBRegistros = new DataSet();
+                BDRegistros.Fill(TBRegistros, "Registros");
+                RegRegistros = TBRegistros.Tables["Registros"].Rows[0];
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No hay ningun Registro en la base de datos!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+                return;
+            }
+
+            for (int i = 0; i < BindingContext[TBRegistros, "Registros"].Count; i++)
+            {
+                string STRSql3 = "SELECT ID,NomCorto FROM Carrera WHERE ID=" + RegRegistros["Carrera"];
+                SqlCommand cmd2 = new SqlCommand(STRSql3, cnn);
+                BDCarrera = new SqlDataAdapter(cmd2);
+                TBCarrera = new DataSet();
+                BDCarrera.Fill(TBCarrera, "Carrera");
+                RegCarrera = TBCarrera.Tables["Carrera"].Rows[0];
+
+                RegRegistros = TBRegistros.Tables["Registros"].Rows[i];
+                Tabla.Rows.Add();
+                Tabla.Rows[i].Cells[0].Value = RegRegistros["Matricula"];
+                Tabla.Rows[i].Cells[1].Value = RegRegistros["Nombres"];
+                Tabla.Rows[i].Cells[2].Value = RegRegistros["Apellido_Paterno"];
+                Tabla.Rows[i].Cells[3].Value = RegRegistros["Apellido_Materno"];
+                Tabla.Rows[i].Cells[4].Value = RegCarrera["NomCorto"];
+                if (Convert.ToInt32(RegRegistros["Semestre"]) == 0)
+                {
+                    Tabla.Rows[i].Cells[5].Value = "-";
+                }
+                else
+                {
+                    Tabla.Rows[i].Cells[5].Value = RegRegistros["Semestre"];
+                }
+
+                Tabla.Rows[i].Cells[6].Value = RegRegistros["Fec_InicioSesion"];
+                Tabla.Rows[i].Cells[7].Value = RegRegistros["Fec_Registro"];
+            }
+        }
+}
